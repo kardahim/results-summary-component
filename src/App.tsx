5bd982ef967389878eea143241d1c992f0a4fa0c
Paweł Ciszewski <kardahim@gmail.com>
2023-08-17 14:30:45 +0200

    left side complete (need minor fixes)

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,17 @@ function App() {
       {/* index page */}
       <div className="card">
         <div className="card__score">
-          <div className="card__score__header"></div>
-          <div className="card__score__circle">
-            <div className="card__score__circle__points"></div>
-            <div className="card__score__circle__total"></div>
+          <div className="card__score__header">
+            your result
           </div>
-          <div className="card__score__header"></div>
-          <p className="card__score__paragraph"></p>
+          <div className="card__score__circle">
+            <span className="card__score__circle__points">{76}</span>
+            <span className="card__score__circle__total">of 100</span>
+          </div>
+          <div className="card__score__paragraph_title">Great</div>
+          <p className="card__score__paragraph">
+            You scored higher thean 65% of the people who have taken these tests.
+          </p>
         </div>
         <div className="card__summary"></div>
       </div>
